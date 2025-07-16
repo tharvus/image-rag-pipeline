aff667e5b1e23f77c3b2b910b83f592949c6da8d
A S Payal Thangamma <payalthangamma99@gmail.com>
2025-07-16 16:13:37 +0200

    clip, blip,siglip short and long captions

--- initialise_vector_database.py
+++ initialise_vector_database.py
@@ -1,22 +1,23 @@
+import math
 from qdrant_client import QdrantClient
 from qdrant_client.models import Distance, VectorParams
 from qdrant_client.models import PointStruct
 from qdrant_client.http import models as rest_models
 
-client = QdrantClient(url="http://localhost:6333")
+def create_collections(dimension):
+        client = QdrantClient(url="http://localhost:6333")
+        client.delete_collection(collection_name="image_db")
 
-client.delete_collection(collection_name="image_db")
+        # initialise collection
+        client.create_collection(
+            collection_name="image_db",
+            vectors_config=VectorParams(size=dimension, distance=Distance.COSINE),
+        )
 
-# initialise collection
-client.create_collection(
-    collection_name="image_db",
-    vectors_config=VectorParams(size=512, distance=Distance.COSINE),
-)
+        client.delete_collection(collection_name="spectral_collection")
 
-client.delete_collection(collection_name="spectral_collection")
-
-# initialise collection
-client.create_collection(
-    collection_name="spectral_collection",
-    vectors_config=VectorParams(size=32, distance=Distance.COSINE),
-)
+        # initialise collection
+        client.create_collection(
+            collection_name="spectral_collection",
+            vectors_config=VectorParams(size=(2 ** (math.floor(math.log2(dimension)))), distance=Distance.COSINE),
+        )
--- spectral_code.py
+++ spectral_code.py
@@ -9,7 +9,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 from scipy.sparse.csgraph import laplacian
 from scipy.linalg import eigh
 
-def compute_spectral_embedding(embeddings, n_components=32):
+def compute_spectral_embedding(embeddings, n_components):
     """
     Compute spectral embedding using normalized graph Laplacian eigenvectors.
     embeddings: numpy array (N x D)
@@ -17,10 +17,14 @@ def compute_spectral_embedding(embeddings, n_components=32):
     """
     # Similarity matrix (cosine similarity)
     similarity = cosine_similarity(embeddings)
+
+    similarity[similarity < 0] = 0
+    np.fill_diagonal(similarity, 1.0 + 1e-9)
     
+
     # Compute normalized Laplacian
     L = laplacian(similarity, normed=True)
-    
+
     # Compute eigenvalues and eigenvectors
     eigvals, eigvecs = eigh(L)
     
